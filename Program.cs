90778237af0f3173b25c9ad3d28cdd801a372930
Jennifer <jnarvaez07@gmail.com>
2021-11-26 21:55:52 -0600

    Activity 10 code debugging

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;//added from code snippet
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity10CodeCorrection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+
+
+        }
+    }
+}
---
+++ Set.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Activity10CodeCorrection
+{
+    class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+
+        }
+        private bool containsElement(int val)
+        {
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i])
+                    return true;
+               /* else
+                    return false;*/
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        public Set union(Set rhs)
+        {
+            Set union = new Set();
+            
+ 
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                union.addElement(rhs.elements[i]);
+            }
+            for (int x = 0; x < this.elements.Count; x++)
+            {
+                union.addElement(this.elements[x]);
+            }
+            return union;
+
+        }
+    }
+}
